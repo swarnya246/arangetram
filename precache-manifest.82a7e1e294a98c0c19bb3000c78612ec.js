20e6f9ec940ccb38e8f8b1accbfdafd3f18e08ed
root <root@Srivastava-Idea-Pad.localdomain>
2020-10-11 08:37:30 -0700

    Updates

---
+++ precache-manifest.82a7e1e294a98c0c19bb3000c78612ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf5348b80353f4e6dc25ec68429254b",
+    "url": "/arangetram/index.html"
+  },
+  {
+    "revision": "dd8e46ac2dc7883ab0b5",
+    "url": "/arangetram/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "c9e01eea37e58314bf7f",
+    "url": "/arangetram/static/css/main.8f29a714.chunk.css"
+  },
+  {
+    "revision": "dd8e46ac2dc7883ab0b5",
+    "url": "/arangetram/static/js/2.335f3e07.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/arangetram/static/js/2.335f3e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e01eea37e58314bf7f",
+    "url": "/arangetram/static/js/main.f3fd488a.chunk.js"
+  },
+  {
+    "revision": "3745e460945c117a18be",
+    "url": "/arangetram/static/js/runtime-main.581f5859.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/arangetram/static/media/logo.5d5d9eef.svg"
+  }
+]);
